f91035c6753dd1607cbd37e5dd4e055ae069e36d
Janay Bailey <tjanay87@gmail.com>
2019-09-18 15:27:33 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["blue", "green", "yellow", "purple"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ("blue", <<(shovel))
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ("green", "blue").to eq("purple")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ["blue", "green", "yellow", "purple"] 
 end
 
 def remove_element_from_start_of_array(array)
