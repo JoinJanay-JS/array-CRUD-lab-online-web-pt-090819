2a4895a519169a36d58f1f637cea8742bfc9e12a
Janay Bailey <tjanay87@gmail.com>
2019-09-18 15:23:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ("green", "blue").to
+  ("green", "blue").to eq("purple")
 end
 
 def remove_element_from_end_of_array(array)
